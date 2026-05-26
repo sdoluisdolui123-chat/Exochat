215f3396e62bbf35bbc438989bc553debbb63628
sdoluisdolui123-chat <sdoluisdolui123@gmail.com>
2026-05-26 09:01:11 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10581,6 +10581,6 @@ def security_info():
 
 # ----------------- Server Run -----------------
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
